eb4958a3d3b387d086d495a9601f382f680b04f5
Gauravrajsingh <er.gauravraj8052625814@gmail.com>
2025-04-27 23:34:41 +0530

    code added

---
+++ Environment/dev/variables.tf
@@ -0,0 +1,17 @@
+################resourcegroup
+variable "rg_details" {}
+
+############storageaccount
+variable "st_details" {}
+
+###########virtualnetwork
+variable "vnet_details" {}
+
+####################subnet
+variable "snet_details" {}
+
+####################virtualmachine
+variable "vms_details" {}
+
+####################bastion
+variable "bastions_details" {}
